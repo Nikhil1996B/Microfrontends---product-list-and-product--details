68a34ae9a935096d57a7541959bd41f374b64b3c
nikhil1996b <30387741+Nikhil1996B@users.noreply.github.com>
2024-01-26 10:51:36 +0000

    Add the webpack module federations plugin to remote (products) and expose the file which the remove (products) needs to expose

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,6 +1,6 @@
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-
+const moduleFederationPlugin = require("webpack/lib/container/moduleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
@@ -8,6 +8,13 @@ module.exports = {
     allowedHosts: ["h78kdq-8081.csb.app"],
   },
   plugins: [
+    new moduleFederationPlugin({
+      name: "products",
+      fileName: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
     new htmlWebpackPlugin({
       template: path.join(__dirname, "/public", "/index.html"),
     }),
